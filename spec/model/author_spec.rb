6cf1c3b9d4a55c64bd4d07b24b22f330721af504
Sunny Moore <moore.sunny@gmail.com>
2022-07-08 15:52:11 -0700

    create author index, user story 1

---
+++ spec/model/author_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe Author do
+  describe 'instance methods' do
+    describe '#deceased' do
+      it 'is false by default' do
+        author = Author.new(name: 'Stephen King')
+
+        expect(author.deceased?).to be false
+      end
+      it 'it is true for deceased?' do
+        author = Author.new(name: 'Charles Dickens', deceased: true)
+
+        expect(author.deceased?).to be true
+      end
+    end
+    describe '#awards' do
+      it 'is nil/zero by default' do
+        author = Author.new(name: 'Stephen King')
+
+        expect(author.awards).to eq(0)
+      end
+      it 'can be set to an integer value' do
+        author = Author.new(name: 'Stephen King', awards: 5)
+
+        expect(author.awards).to eq(5)
+      end
+    end
+    describe '#name' do
+      it 'you can specify a name' do
+        author = Author.new(name: 'Stephen King')
+
+        expect(author.name).to eq('Stephen King')
+      end
+    end
+  end
+end
