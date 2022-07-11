7b657d0b0abf895e083e96765e191f68b2490bb8
Sunny Moore <moore.sunny@gmail.com>
2022-07-11 12:06:18 -0700

    add books table and test instance variables

---
+++ spec/model/book_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe Book do 
+  describe 'instance methods' do
+    describe '#name' do
+      it 'you can specify a name' do
+        book = Book.new(name: 'The Stand')
+
+        expect(book.name).to eq('The Stand')
+      end
+      describe '#in_print?' do 
+        it 'is false by default' do
+          book = Book.new(name: 'The Stand')
+
+          expect(book.in_print?).to eq(false)
+        end
+        it 'can be set to true' do
+          book = Book.new(name: 'The Stand', in_print: true)
+          
+          expect(book.in_print?).to eq(true)
+        end
+
+        describe '#length' do
+          it 'is zero by default' do
+            book = Book.new(name: 'The Stand')
+
+            expect(book.length).to eq(0)
+          end
+          it 'can be set to an integer value' do
+            book = Book.new(name: 'The Stand', length: 1152)
+
+            expect(book.length).to eq(1152)
+          end
+        end
+      end
+    end
+  end
+end
