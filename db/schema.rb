d6729991ffc2b9e6b33d84f36c05c443826661ae
Sunny Moore <moore.sunny@gmail.com>
2022-07-11 17:56:52 -0700

    add foreign key to books of author_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_11_213241) do
+ActiveRecord::Schema.define(version: 2022_07_12_005331) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,9 @@ ActiveRecord::Schema.define(version: 2022_07_11_213241) do
     t.boolean "in_print", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "author_id"
+    t.index ["author_id"], name: "index_books_on_author_id"
   end
 
+  add_foreign_key "books", "authors"
 end
