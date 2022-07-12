13e3eadb0fea4552058999b2092baa5262b2d3f4
Sunny Moore <moore.sunny@gmail.com>
2022-07-12 10:08:48 -0700

    test model method count_books, create model instance method count_books

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,11 +1,15 @@
 class Author < ApplicationRecord
-    validates_presence_of :name
-    validates_presence_of :awards
-    validates :deceased, inclusion: [true, false]
+  validates_presence_of :name
+  validates_presence_of :awards
+  validates :deceased, inclusion: [true, false]
 
-    has_many :books
+  has_many :books
 
-    def self.order_by_created_at
-        Author.order(created_at: :desc)
-    end
+  def self.order_by_created_at
+    Author.order(created_at: :desc)
+  end
+  
+  def count_books
+    books.count
+  end
 end
--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -19,5 +19,16 @@ RSpec.describe Author do
 
       expect(authors.first.name).to eq('Charles Dickens')
     end
+    it 'can count books associated with an Author (user story 7)' do
+      author1 = Author.create!(name: 'Stephen King', awards: 27, deceased: false)
+      author2 = Author.create!(name: 'Charles Dickens', awards: 5, deceased: true)
+      book1 = author1.books.create!(name: 'The Stand', length: 1152, in_print: true)
+      book2 = author1.books.create!(name: 'It', length: 1138, in_print: true)
+      book3 = author1.books.create!(name: 'Carrie', length: 199, in_print: false)
+      book4 = author2.books.create!(name: 'A Tale of Two Cities', length: 448, in_print: false)
+
+      expect(author1.count_books).to eq(3)
+      expect(author2.count_books).to eq(1)
+    end
   end
 end
