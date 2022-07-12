9865bdcb0236b0f564e7fb1c19bad32869d45f93
Sunny Moore <moore.sunny@gmail.com>
2022-07-11 19:07:03 -0700

    test author_books_index page, add route, controller view

--- spec/features/authors/books/index_spec.rb
+++ spec/features/authors/books/index_spec.rb
@@ -8,16 +8,16 @@
 require 'rails_helper'
 
 RSpec.describe 'Authors Books Index' do
-  it 'when I visit /authors/:id/books' do
+  it 'when I visit /authors/:author_id/books' do
     author = Author.create!(name: 'Stephen King')
     author2 = Author.create!(name: 'Charles Dickens')
     book1 = author.books.create!(name: 'The Stand', length: 1152, in_print: true)
     book2 = author.books.create!(name: 'It', length: 1138, in_print: true)
     book3 = author.books.create!(name: 'Carrie', length: 199, in_print: false)
-    book4 = author.books.create!(name: 'A Tale of Two Cities', length: 448, in_print: false)
+    book4 = author2.books.create!(name: 'A Tale of Two Cities', length: 448, in_print: false)
 
     visit "/authors/#{author.id}/books"
-
+save_and_open_page
     expect(page).to have_content(book1.name)
     expect(page).to have_content(book2.name)
     expect(page).to have_content(book3.name)
