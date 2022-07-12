3ed114081927beb3a064c60bfa75410efff93092
Sunny Moore <moore.sunny@gmail.com>
2022-07-12 12:59:51 -0700

    test each page has link to authors index, add link to application.html.erb

--- spec/features/authors/index_spec.rb
+++ spec/features/authors/index_spec.rb
@@ -1,33 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe 'authors#index', type: :feature do
-  describe 'User Story 1' do
-    it 'can show all the authors' do
-      author1 = Author.create!(name: 'Stephen King')
-      author2 = Author.create!(name: 'Charles Dickens')
-      visit '/authors'
 
-      expect(page).to have_content(author1.name)
-      expect(page).to have_content(author2.name)
-    end
+  it 'can show all the authors (User Story 1)' do
+    author1 = Author.create!(name: 'Stephen King')
+    author2 = Author.create!(name: 'Charles Dickens')
+    visit '/authors'
+
+    expect(page).to have_content(author1.name)
+    expect(page).to have_content(author2.name)
   end
-  describe 'User Story 6' do
-    it 'shows all authors, sorted by most recently created' do 
-      author1 = Author.create!(name: 'Stephen King')
-      author2 = Author.create!(name: 'Charles Dickens')
 
-      visit '/authors'
-      
-      expect(page.body.index(author2.name)).to be < (page.body.index(author1.name))
-    end
+  it 'shows all authors, sorted by most recently created(User Story 6)' do 
+    author1 = Author.create!(name: 'Stephen King')
+    author2 = Author.create!(name: 'Charles Dickens')
+
+    visit '/authors'
+    
+    expect(page.body.index(author2.name)).to be < (page.body.index(author1.name))
   end
-  describe 'User Story 7' do
-    xit 'authors show page shows a count of all books associated with it' do
-      author1 = Author.create!(name: 'Stephen King')
-      book1 = author.books.create!(name: 'The Stand', length: 1152, in_print: true)
-      book2 = author.books.create!(name: 'It', length: 1138, in_print: true)
 
-      visit "/authors/#{author.id}"
-    end
+  it 'every page has a link to Authors Index(User Story 9)' do
+    author1 = Author.create!(name: 'Stephen King')
+
+    visit "/authors/#{author1.id}"
+    expect(page).to have_content('Authors Index')
+    visit "/authors/"
+    expect(page).to have_content('Authors Index')
   end
 end
