087975be90da99c8fab58abac4e74bc32c11d593
Sunny Moore <moore.sunny@gmail.com>
2022-07-11 17:49:14 -0700

    Fixed shoulda matcher for in_print boolean

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ApplicationRecord
   validates_presence_of :name
   validates_presence_of :length
-  validates_presence_of :in_print
+  validates_inclusion_of :in_print, in: [true, false]
 end
