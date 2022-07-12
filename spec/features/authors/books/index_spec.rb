4aff58a124c8e668df351c27ccf7a90bd9b6806f
Sunny Moore <moore.sunny@gmail.com>
2022-07-11 18:45:57 -0700

    create authors books index page test

---
+++ spec/features/authors/books/index_spec.rb
@@ -0,0 +1,26 @@
+# User Story 5, Authors Books Index 
+
+# As a visitor
+# When I visit '/authors/:id/books'
+# Then I see each Book that is associated with that Author 
+# with each Books's attributes:
+
+require 'rails_helper'
+
+RSpec.describe 'Authors Books Index' do
+  it 'when I visit /authors/:id/books' do
+    author = Author.create!(name: 'Stephen King')
+    author2 = Author.create!(name: 'Charles Dickens')
+    book1 = author.books.create!(name: 'The Stand', length: 1152, in_print: true)
+    book2 = author.books.create!(name: 'It', length: 1138, in_print: true)
+    book3 = author.books.create!(name: 'Carrie', length: 199, in_print: false)
+    book4 = author.books.create!(name: 'A Tale of Two Cities', length: 448, in_print: false)
+
+    visit "/authors/#{author.id}/books"
+
+    expect(page).to have_content(book1.name)
+    expect(page).to have_content(book2.name)
+    expect(page).to have_content(book3.name)
+    expect(page).to_not have_content(book4.name)
+  end
+end
