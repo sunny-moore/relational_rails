5dcf26f01cb1127308421368921d898d579f932b
Sunny Moore <moore.sunny@gmail.com>
2022-07-13 11:52:45 -0700

    Refactor for author book relationship

--- spec/features/books/index_spec.rb
+++ spec/features/books/index_spec.rb
@@ -8,34 +8,32 @@ require 'rails_helper'
 
 RSpec.describe 'As a visitor' do
   describe 'when I visit /books' do
+    before :each do
+      @author = Author.create!(name: 'Stephen King', deceased: false, awards: 10)
+      @book1 = @author.books.create!(name: 'The Stand', length: 1152, in_print: true)
+      @book2 = @author.books.create!(name: 'It', length: 1138, in_print: true)
+    end
     it 'can see each book in the system with its attributes' do
-      author = Author.create!(name: 'Stephen King', deceased: false, awards: 10)
-      book1 = author.books.create!(name: 'The Stand', length: 1152, in_print: true)
-      book2 = author.books.create!(name: 'It', length: 1138, in_print: true)
-      
       visit '/books'
 
       expect(current_path).to eq('/books')
-
-      expect(page).to have_content(book1.name)
-      expect(page).to have_content(book1.length)
-      expect(page).to have_content(book1.in_print)
-      expect(page).to have_content(book2.name)
-      expect(page).to have_content(book2.length)
-      expect(page).to have_content(book2.in_print)
+      expect(page).to have_content(@book1.name)
+      expect(page).to have_content(@book1.length)
+      expect(page).to have_content(@book1.in_print)
+      expect(page).to have_content(@book2.name)
+      expect(page).to have_content(@book2.length)
+      expect(page).to have_content(@book2.in_print)
     end
   end
   it 'shows a link to books index on every page(User Story 8)' do
-    author = Author.create!(name: 'Stephen King', deceased: false, awards: 10)
-    book1 = author.books.create!(name: 'The Stand', length: 1152, in_print: true)
-    book2 = author.books.create!(name: 'It', length: 1138, in_print: true)
     visit '/books'
     expect(page).to have_link('Books Index')
 
     visit '/authors'
     expect(page).to have_link('Books Index')
 
-    visit "/authors/#{author.id}"
+    author2 = Author.create!(name: 'Stephen King', deceased: false, awards: 10)
+    visit "/authors/#{author2.id}"
     expect(page).to have_link('Books Index')
   end
 end
